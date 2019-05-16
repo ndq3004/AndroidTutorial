daa096cee11d75fa86c7a5b1ce79789d01620fe4
ndq3004 <ndq3004@gmail.com>
2019-05-16 22:00:10 +0700

    add project

---
+++ app/src/main/java/android/lunchlist/Restaurant.java
@@ -0,0 +1,45 @@
+package android.lunchlist;
+
+public class Restaurant {
+    private String name="";
+    private String address="";
+    private String type="";
+    private String notes=null;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String toString(){
+        return (getName());
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
